312a009b726a8e3e9822278b67613e3b77b92903
Keyvan Akbary <kiwwito@gmail.com>
2015-01-16 16:21:19 +0000

    Allow extending the listener with an ignore policy

--- src/TestListener.php
+++ src/TestListener.php
@@ -4,27 +4,33 @@ namespace MyBuilder\PhpunitAccelerator;
 
 class TestListener implements \PHPUnit_Framework_TestListener
 {
+    private $ignorePolicy;
+
     const PHPUNIT_PROPERTY_PREFIX = 'PHPUnit_';
+
+    public function __construct(IgnoreTestPolicy $ignorePolicy = null)
+    {
+        $this->ignorePolicy = ($ignorePolicy) ?: new NeverIgnoreTestPolicy();
+    }
 
     public function endTest(\PHPUnit_Framework_Test $test, $time)
     {
-        $this->safelyFreeProperties($test);
+        $testReflection = new \ReflectionObject($test);
+
+        if ($this->ignorePolicy->shouldIgnore($testReflection)) {
+            return;
+        }
+
+        $this->safelyFreeProperties($test, $testReflection->getProperties());
     }
 
-    private function safelyFreeProperties(\PHPUnit_Framework_Test $test)
+    private function safelyFreeProperties(\PHPUnit_Framework_Test $test, array $properties)
     {
-        foreach ($this->getProperties($test) as $property) {
+        foreach ($properties as $property) {
             if ($this->isSafeToFreeProperty($property)) {
                 $this->freeProperty($test, $property);
             }
         }
-    }
-
-    private function getProperties(\PHPUnit_Framework_Test $test)
-    {
-        $reflection = new \ReflectionObject($test);
-
-        return $reflection->getProperties();
     }
 
     private function isSafeToFreeProperty(\ReflectionProperty $property)
@@ -58,4 +64,12 @@ class TestListener implements \PHPUnit_Framework_TestListener
     public function startTest(\PHPUnit_Framework_Test $test) {}
 
     public function addRiskyTest(\PHPUnit_Framework_Test $test, \Exception $e, $time) {}
+}
+
+class NeverIgnoreTestPolicy implements IgnoreTestPolicy
+{
+    public function shouldIgnore(\ReflectionObject $testReflection)
+    {
+        return false;
+    }
 }
